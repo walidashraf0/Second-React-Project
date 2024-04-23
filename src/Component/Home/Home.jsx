37f04e88941e7b03cbbdf69793673fb09ec13c2c
walid <walidashraf050@gmail.com>
2024-04-24 01:24:37 +0200

    react helmet edit

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import CategorySlider from '../CategorySlider/CategorySlider';
 import FeaturedProducts from '../FeaturedProducts/FeaturedProducts';
 import MainSlider from '../MainSlider/MainSlider';
+import { Helmet } from 'react-helmet';
+
 
 
 export default function Home() {
@@ -9,8 +11,13 @@ export default function Home() {
 
     return <>
 
-    <MainSlider />
-    <CategorySlider />
+        <Helmet>
+            <meta name='description' content='' />
+            <title>WEGO Home</title>
+        </Helmet>
+
+        <MainSlider />
+        <CategorySlider />
         <FeaturedProducts />
     </>
 }
--- src/Component/ProductDetails/ProductDetails.jsx
+++ src/Component/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,7 @@ import Style from './ProductDetails.module.css';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import axios from 'axios';
+import { Helmet } from 'react-helmet';
 
 
 
@@ -14,33 +15,40 @@ export default function ProductDetails() {
         return axios.get(`https://ecommerce.routemisr.com/api/v1/products/${id}`)
     }
 
-    let { isLoading, isError, data } = useQuery('productDetails', ()=> getProductDetails(params.id))
+    let { isLoading, isError, data } = useQuery('productDetails', () => getProductDetails(params.id))
     console.log(data?.data.data);
-    
+
 
 
     return <>
-    {data?.data.data? <div className='row py-2 align-items-center'>
-        <div className="col-md-4">
-            <img className='w-100' src={data?.data.data.imageCover} alt={data?.data.data.title} />
-        </div>
-        <div className="col-md-8 ">
-            <h2 className='h5'>{data?.data.data.title}</h2>
-            <p>{data?.data.data.description}</p>
 
-            <h6 className='text-main'>{data?.data.data.category?.name}</h6>
-            <h6 className='text-main'>Price {data?.data.data.price} EGP</h6>
-            <div className='d-flex justify-content-between'>
-                <span>ratingQuantity: {data?.data.data.ratingsQuantity}</span>
-                <span><i className='fa fa-star rating-color'>{data?.data.data.ratingsAverage}</i></span>
+        
+
+        {data?.data.data ? <div className='row py-2 align-items-center'>
+        <Helmet>
+            <meta  name='description' content='' />
+            <title>{data?.data.data.title}</title>
+        </Helmet>
+            <div className="col-md-4">
+                <img className='w-100' src={data?.data.data.imageCover} alt={data?.data.data.title} />
+            </div>
+            <div className="col-md-8 ">
+                <h2 className='h5'>{data?.data.data.title}</h2>
+                <p>{data?.data.data.description}</p>
+
+                <h6 className='text-main'>{data?.data.data.category?.name}</h6>
+                <h6 className='text-main'>Price {data?.data.data.price} EGP</h6>
+                <div className='d-flex justify-content-between'>
+                    <span>ratingQuantity: {data?.data.data.ratingsQuantity}</span>
+                    <span><i className='fa fa-star rating-color'>{data?.data.data.ratingsAverage}</i></span>
+                </div>
+
+                <button className='btn bg-main text-white w-100 mt-2'>Add To Cart</button>
+
             </div>
 
-            <button className='btn bg-main text-white w-100 mt-2'>Add To Cart</button>
 
-        </div>
-
-
-    </div> : ''}
-    <h2>ProductDetails</h2>
+        </div> : ''}
+        <h2>ProductDetails</h2>
     </>
 }
