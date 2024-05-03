a9759144568d2eaaef53261ac1eda12104466aa1
walid <walidashraf050@gmail.com>
2024-05-04 00:48:31 +0300

    useRef

--- src/Component/Categories/Categories.jsx
+++ src/Component/Categories/Categories.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Style from './Categories.module.css';
 
 
 export default function Categories() {
 
+    const [counter, setCounter] = useState(0);
+
+    let render = useRef(0);
+    let myInput = useRef();
+
+    useEffect(()=> {
+        render.current +=1;
+        myInput.current.focus();
+    }, [])
+
+
     
     return <>
     <h2>Categories</h2>
+    <h3>Counter: {counter}</h3>
+    <h3>Render: {render.current}</h3>
+    <input ref={myInput} />
+    <button onClick={()=> setCounter(counter+1)} className='btn m-2 btn-info text-white'>+</button>
+    <button onClick={()=> setCounter(counter-1)} className='btn m-2 btn-info text-white'>-</button>
     </>
 }
